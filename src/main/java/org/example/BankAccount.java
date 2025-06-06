045f9d9eb19d24597f1f50437fa83484aedea724
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-06-06 19:23:49 -0230

    Made a bank account that keeps track of money, lets you deposit and withdraw, and check the balance

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder) {
+        this.accountHolder = accountHolder;
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+}
