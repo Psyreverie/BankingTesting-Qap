8ae309304e9f0810ae13c9272ae076dfe478b778
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-06-09 14:36:05 -0230

    Added bank account tests and tested them.

--- src/test/java/org/example/BankAccountTest.java
+++ src/test/java/org/example/BankAccountTest.java
@@ -1,4 +1,30 @@
 package org.example;
 
-public class BankAccountTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void testDeposit() {
+        BankAccount account = new BankAccount("Zackery");
+        account.deposit(100);
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        BankAccount account = new BankAccount("Zackery");
+        account.deposit(200);
+        account.withdraw(50);
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    void testWithdrawInsufficientFunds() {
+        BankAccount account = new BankAccount("Zackery");
+        account.deposit(100);
+        account.withdraw(150);
+        assertEquals(100, account.getBalance());
+    }
 }
